e4c5179e7ec6021057f005243f78cc9e9160b879
yueyang2000 <m18519660881@163.com>
2020-11-10 19:00:47 +0800

    change clk back to 50M

--- thinpad_top.srcs/sources_1/new/thinpad_top.v
+++ thinpad_top.srcs/sources_1/new/thinpad_top.v
@@ -103,7 +103,7 @@ always@(posedge clk_20M or negedge locked) begin
     else        reset_of_clk20M <= 1'b0;
 end
 
-wire clk = clk_11M0592;
+wire clk = clk_50M;
 wire rst = reset_btn;
 
 reg oe_uart_n, we_uart_n;
